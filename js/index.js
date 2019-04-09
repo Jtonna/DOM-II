50b75b5d67b16f31fe450b212fa255db350c1b24
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-04-09 16:13:53 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,7 +12,44 @@ const logo = document.querySelector('.logo-heading');
 logo.addEventListener('mouseover', () => { 
     logo.style.color = 'red';
     // reset the color after a short delay
-    setTimeout(function() {
+    setTimeout(() => {
         logo.style.color = 'green';
       }, 1500);
-    }, false);
+});
+
+// key down
+
+const backgroundColor = document.querySelector('html');
+backgroundColor.addEventListener('keydown', () => {
+    backgroundColor.style.background = '#F7AEF8';
+    // adds a color gradient on reset
+    setTimeout(() => {
+        backgroundColor.style.background = "lightgreen";
+        alert ("Damnit, you broke something. Click \"OK\" to fix it!");
+    }, 100);
+    
+    // changes colors 
+    setInterval( ()=> {
+        backgroundColor.style.background = '#8093F1';
+    }, 200);
+    setInterval( ()=> {
+        backgroundColor.style.background = '#F43B38';
+    }, 300);
+    setInterval ( ()=>{
+        backgroundColor.style.background = '#F7AEF8';
+    }, 400);
+    setInterval ( ()=> {
+        backgroundColor.style.background = '#72DDF7';
+    }, 500);
+    setTimeout (()=>{
+        alert ("Yeah, you really broke things... click \"OK\" and then press the SPACEBAR to fix it!")
+    },10000);
+});
+
+// Custom Key Down
+backgroundColorBody = document.querySelector('body');
+backgroundColorBody.addEventListener('keydown', function(){
+    if (event.keyCode == '32') {
+        backgroundColorBody.style.background = "white";
+    }
+});
