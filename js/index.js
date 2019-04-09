211b9695cb7138143c9a501724a9ce1b19c02a47
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-04-09 14:52:43 -0700

    updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const siteContent = {
+    '.main-navagation': {
+      'nav-item-1': 'Services',
+      '': '',
+    }
+}
+
+// mouse over
+
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', () => { 
+    logo.style.color = 'red';
+    // reset the color after a short delay
+    setTimeout(function() {
+        logo.style.color = 'green';
+      }, 1500);
+    }, false);
